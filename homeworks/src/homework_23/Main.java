bc15c72f35591e1d2f375225de1081dd0bb0b1d8
EsenDzhumaev1 <esendzhumaev@gmail.com>
2021-05-20 20:18:44 +0600

    homework #23(completed#2)

--- homeworks/src/homework_23/Main.java
+++ homeworks/src/homework_23/Main.java
@@ -7,8 +7,8 @@ public class Main extends Canvas{
     int width;
     int height;
     public Main(){
-        this.width = 960;
-        this.height = 720;
+        this.width = 900;
+        this.height = 900;
     }
     public void section(Graphics g) {
         g.setColor(Color.BLACK);
@@ -19,7 +19,7 @@ public class Main extends Canvas{
     public void graphic(Graphics g, int degree, Color clr) {
         g.setColor(clr);
         System.out.println("New Graphic\n" + "=".repeat(30) + "\n");
-        for (double x = -100; x <= 100; x += 0.1){
+        for (double x = -10; x <= 10; x += 0.1){
             double y = Math.pow(x, degree);
             int X1 = (int)Math.round((double)width / 2 + x * (double) (width / 20));
             int Y1 = (int)Math.round((double)height / 2 - y * (double) (height / 20));
